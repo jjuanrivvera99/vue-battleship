7fb853704d88aa12a97adeb4a6d80f846f89dff8
Juan Felipe Rivera G <juan.rivera@alegra.com>
2021-11-28 23:30:11 -0500

    Lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
   extends: [
     "plugin:vue/recommended",
     "eslint:recommended"
+    // "@vue/prettier"
   ],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
